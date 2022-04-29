e97c273996d82415031320313202a384478d46fd
TshivhaseNdiene <tshivhasen97@outlook.com>
2022-04-29 15:41:07 +0300

    little update

--- myprofile/assets/js/app.js
+++ myprofile/assets/js/app.js
@@ -29,12 +29,3 @@ scrollUp.addEventListener("click", () => {
     behavior: "smooth",
   });
 });
-
-//Download Button
-function download(fileUrl, fileName){
-  var a = document.createElement("a");
-  a.href = fileUrl;
-  a.setAttribute("download" fileName);
-  a.click();
-}
-download("cv/ndiene.pdf", "ndiene.pdf-");
